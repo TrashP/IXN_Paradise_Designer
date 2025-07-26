c1488d5fcc6b3d7ea5a863e5958b5aa5307ad72e
patlei <jingyi021002@163.com>
2025-07-26 14:23:41 +0100

    added the home page

---
+++ Assets/Scripts/StartGameDialogController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGameDialogController : MonoBehaviour
+{
+    [Header("Panel")]
+    public GameObject startGamePanel;
+
+    [Header("Buttons")]
+    public Button btnBuildMap;
+    public Button btnChooseMap;
+    public Button btnClose;
+
+    void Start()
+    {
+        startGamePanel.SetActive(false);
+    }
+
+    void OnEnable()
+    {
+        if (btnBuildMap != null)
+            btnBuildMap.onClick.AddListener(OnBuildMapClicked);
+        if (btnChooseMap != null)
+            btnChooseMap.onClick.AddListener(OnChooseMapClicked);
+        if (btnClose != null)
+            btnClose.onClick.AddListener(HidePanel);
+    }
+
+    public void ShowPanel()
+    {
+        if (startGamePanel != null)
+            startGamePanel.SetActive(true);
+    }
+
+    public void HidePanel()
+    {
+        if (startGamePanel != null)
+            startGamePanel.SetActive(false);
+    }
+
+    void OnBuildMapClicked()
+    {
+        Debug.Log("Build your own map clicked!");
+        // SceneManager.LoadScene("DrawingScene");
+    }
+
+    void OnChooseMapClicked()
+    {
+        Debug.Log("Choose pre-designed map clicked!");
+        // SceneManager.LoadScene("SelectMapScene");
+    }
+}
